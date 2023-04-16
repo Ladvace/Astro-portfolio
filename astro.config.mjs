619968b8c7493ed27ee21cede9a6165273d90118
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-04-16 16:41:54 +0200

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import solidJs from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), solidJs()]
+  integrations: [tailwind(), sitemap(), solidJs()],
+  output: "server",
+  adapter: vercel()
 });
