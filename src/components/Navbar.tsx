8d0bab0a1f34e79cc1e56f7d2d7b011d272e68a0
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-05-07 21:22:06 +0200

    readme and navbar hover animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,7 +51,7 @@ const Navbar = (props: Props) => {
                   class="cursor-pointer lg:after:content-[''] lg:after:block lg:after:w-2 lg:after:h-2 lg:after:mt-1 lg:after:rounded-full lg:after:my-0 lg:after:mx-auto"
                   classList={{
                     "lg:after:bg-red-500": props.currentPath === route.slug,
-                    "opacity-50 hover:after:bg-red-500":
+                    "opacity-50 hover:after:bg-red-500 after:scale-0 hover:after:scale-100 after:transition-all after:duration-300 after:ease-in-out":
                       props.currentPath !== route.slug,
                   }}
                 >
