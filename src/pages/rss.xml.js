9b4f6baada6c9ac6563304a6474e919fe309626b
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-05-20 18:23:44 +0200

    pubDate test

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function get(context) {
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
-      pubDate: post.data.publishDate,
+      pubDate: post.data.pubDate,
       description: post.data.description,
       // Compute RSS link from post `slug`
       // This example assumes all posts are rendered as `/blog/[slug]` routes
