d6547f153c48849111428feeb670aff5618eba57
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-05-20 18:31:04 +0200

    date

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { z, defineCollection } from 'astro:content';
 const blog = defineCollection({
     schema: z.object({
       title: z.string(),
-      pubDate: z.string(),
+      pubDate: z.date(),
       description: z.string(),
       link: z.string(),
       img: z.object({
